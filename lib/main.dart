2a6ec88a2ea2a1e58a4a11e4e2d755e488238212
limzzum <chfhddl1234@naver.com>
2021-07-07 12:55:14 +0900

    console  창으로 확인 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 
+import 'dart:async';
+import 'dart:convert';
+import 'dart:developer';
+//import 'dart:html';
 import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:http/http.dart' as http;
+//import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-void main() {
+
+void main() async{
+
+
   runApp(MyApp());
 }
 
@@ -14,15 +25,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -39,26 +42,132 @@ class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
-
-class _MyHomePageState extends State<MyHomePage> {
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   // Enable hybrid composition.
-  //   if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
-  // }
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-
-      body: SafeArea(
-        child: WebView(
-          initialUrl: 'https://flutter.dev',
-          javascriptMode: JavascriptMode.unrestricted,
-        ),
-      ),
-
+class Post{
+  final int access_token;
+  final int refresh_token;
+  Post({required this.access_token,required this.refresh_token});
+  factory Post.fromJson(Map<String,dynamic>json){
+    return Post(
+      access_token: json['access_token'],
+      refresh_token: json['refresh_token']
     );
   }
 }
+class _MyHomePageState extends State<MyHomePage> {
+late InAppWebViewController controller;
+
+Future<Post> fetchPost() async{
+  final response=await http.get(Uri.https('http://192.168.0.121:8080/start','/posts/1'));
+  if (response.statusCode==200){
+    return Post.fromJson(json.decode(response.body));
+
+  }
+  else{
+    throw Exception('Failed to load post');
+  }
+
+}
+
+
+  // Future<bool> ?_onBackPressed() {
+  //
+  //   var future = _webViewController.canGoBack();
+  //   future.then((canGoBack) {
+  //     if (canGoBack) {
+  //       _webViewController.goBack();
+  //     }
+  //     else {
+  //
+  //   return showDialog(
+  //       context: context,
+  //       builder: (context) =>
+  //           AlertDialog(
+  //             title: Text("Do you want to exit the app?"),
+  //             actions: <Widget>[
+  //               FloatingActionButton(
+  //                 child: Text("NO"),
+  //                 onPressed: () => Navigator.pop(context, false),
+  //
+  //               ),
+  //               FloatingActionButton(
+  //                 child: Text("yes"),
+  //                 onPressed: () =>  Navigator.pop(context, true),
+  //
+  //
+  //               )
+  //             ],
+  //           )
+  //   );
+  //
+  //
+  // }
+  //   });
+  //
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      // child: Web(
+
+        body: SafeArea(
+            child: InAppWebView(
+                initialUrlRequest:
+                URLRequest(url: Uri.parse("http://192.168.0.121:8080/start")),
+               
+               // initialHeaders: {},
+                onWebViewCreated: (InAppWebViewController w) {
+                  controller=w;
+
+                  controller.addJavaScriptHandler(
+                      handlerName: "Print", callback: (args) {
+                    print("from java:");
+                    print(args);
+                    //return args;
+                  });
+
+
+                  },
+                onConsoleMessage:
+                    (controller, consoleMessage) {
+                  log("zzConsole:$consoleMessage");
+                  print(consoleMessage);
+                }
+
+            )
+        )
+    );
+  }
+
+
+}
+
+
+
+
+
+
+
+
+
+      //   ),
+      // );
+      // onWillPop: () {
+      //
+      //   var future = _webViewController.canGoBack();
+      //   future.then((canGoBack) {
+      //     if (canGoBack) {
+      //       _webViewController.goBack();
+      //
+      //     }
+      //     else {
+      //       print("종료");
+      //
+      //     }
+      //   }
+      //
+      //   );
+      //   return Future.value(false);
+      // },);
+//   }
